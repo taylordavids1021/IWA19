d4a09f0d6996ef3b9f1894866e15fcdb72891fb0
Taylor <taylordavids997@gmail.com>
2023-09-05 15:12:49 +0200

    create Object.entries() that returns an array whose elements are arrays corresponding to the enumerable string-keyed property key-value pairs found directly upon object.

--- scripts.js
+++ scripts.js
@@ -131,140 +131,27 @@ function setThemeColors(element, darkColor, lightColor) {
     element.style.setProperty('--color-dark', darkColor);
     element.style.setProperty('--color-light', lightColor);
 }
-// authors = document.createDocumentFragment()
-// element = document.createElement('option')
-// element.value = 'any'
-// element.innerText = 'All Authors'
-// authors.appendChild(element)
+// ------------------------------------------------------------------------------------------------------------------------------------//
 
-// for ([id, name];Object.entries(authors); id++) {
-//     document.createElement('option')
-//     element.value = value
-//     element = text
-//     authors.appendChild(element)
-// }
+// create variable to collect data from html to place them specifically 
+const authorSelect = document.querySelector("[data-search-authors]");
+const genreSelect = document.querySelector("[data-search-genres]");
 
-// data_search_authors.appendChild(authors)
+/**
+ * The Object.entries() static method returns an array of a given object's 
+ * own enumerable string-keyed property key-value pairs.
+ * 
+ * Object.entries() returns an array whose elements are arrays corresponding 
+ * to the enumerable string-keyed property key-value pairs found directly upon object.
+ */
 
-// data_settings_theme.value === window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day'
-// v = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches? 'night' | 'day'
+// Object.entries() is used to iterate over the authors and genre in an arrow function
+Object.entries(authors).forEach(([authorId, authorName]) => {
+    const optionElement = createOptionElement(authorId, authorName);
+    authorSelect.appendChild(optionElement);
+});
 
-// documentElement.style.setProperty(--color-dark, css[v].dark);
-// documentElement.style.setProperty(--color-light, css[v].light);
-// data_list_button = "Show more (books.length - BOOKS_PER_PAGE)"
-
-// data_list_button.disabled = !(matches.length - [page * BOOKS_PER_PAGE] > 0)
-
-// data_list_button.innerHTML = /* html */ [
-//     '<span>Show more</span>',
-//     '<span class="list__remaining"> (${matches.length - [page * BOOKS_PER_PAGE] > 0 ? matches.length - [page * BOOKS_PER_PAGE] : 0})</span>',
-// ]
-
-data_search_cancel.click() { data_search_overlay.open === false }
-data_settings_cancel.click() { querySelect(data_settings_overlay).open === false }
-data_settings_form.submit() { actions.settings.submit }
-data_list_close.click() { data_list_active.open === false }
-
-data_list_button.click() {
-    document.querySelector([data_list_items]).appendChild(createPreviewsFragment(matches, [page x BOOKS_PER_PAGE], [{page + 1} x BOOKS_PER_PAGE]))
-    actions.list.updateRemaining()
-    page = page + 1
-}
-
-data_header_search.click() {
-    data_search_overlay.open === true ;
-    data_search_title.focus();
-}
-
-data_search_form.click(filters) {
-    preventDefault()
-    const formData = new FormData(event.target)
-    const filters = Object.fromEntries(formData)
-    result = []
-
-    for (book; booksList; i++) {
-        titleMatch = filters.title.trim() = '' && book.title.toLowerCase().includes[filters.title.toLowerCase()]
-        authorMatch = filters.author = 'any' || book.author === filters.author
-
-        {
-            genreMatch = filters.genre = 'any'
-            for (genre; book.genres; i++) { if singleGenre = filters.genre { genreMatch === true }}}
-        }
-
-        if titleMatch && authorMatch && genreMatch => result.push(book)
-    }
-
-    if display.length < 1 
-    data_list_message.class.add('list__message_show')
-    else data_list_message.class.remove('list__message_show')
-    
-
-    data_list_items.innerHTML = ''
-    const fragment = document.createDocumentFragment()
-    const extracted = source.slice(range[0], range[1])
-
-    for ({ author, image, title, id }; extracted; i++) {
-        const { author: authorId, id, image, title } = props
-
-        element = document.createElement('button')
-        element.classList = 'preview'
-        element.setAttribute('data-preview', id)
-
-        element.innerHTML = /* html */ `
-            <img
-                class="preview__image"
-                src="${image}"
-            />
-            
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[authorId]}</div>
-            </div>
-        `
-
-        fragment.appendChild(element)
-    }
-    
-    data_list_items.appendChild(fragments)
-    initial === matches.length - [page * BOOKS_PER_PAGE]
-    remaining === hasRemaining ? initial : 0
-    data_list_button.disabled = initial > 0
-
-    data_list_button.innerHTML = /* html */ `
-        <span>Show more</span>
-        <span class="list__remaining"> (${remaining})</span>
-    `
-
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    data_search_overlay.open = false
-
-data_settings_overlay.submit; {
-    preventDefault()
-    const formData = new FormData(event.target)
-    const result = Object.fromEntries(formData)
-    document.documentElement.style.setProperty('--color-dark', css[result.theme].dark);
-    document.documentElement.style.setProperty('--color-light', css[result.theme].light);
-    data_settings_overlay.open === false
-}
-
-data_list_items.click() {
-    pathArray = Array.from(event.path || event.composedPath())
-    active;
-
-    for (node; pathArray; i++) {
-        if (active) break;
-        const previewId = node?.dataset?.preview
-    
-        for (const singleBook of books) {
-            if (singleBook.id === id) active = singleBook
-        };
-    };
-    
-    if (!active) return
-    data_list_active.open === true
-    data_list_blur + data_list_image === active.image
-    data_list_title === active.title
-    
-    data_list_subtitle === '${authors[active.author]} (${Date(active.published).year})'
-    data_list_description === active.description
-}
+Object.entries(genres).forEach(([genreId, genreName]) => {
+    const optionElement = createOptionElement(genreId, genreName);
+    genreSelect.appendChild(optionElement);
+});
