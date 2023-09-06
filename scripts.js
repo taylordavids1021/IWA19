7b4bc5976cbadc3acf0a22fbbcd92d7709e43063
Taylor <taylordavids997@gmail.com>
2023-09-06 11:18:15 +0200

    add comments to code for more understanding

--- scripts.js
+++ scripts.js
@@ -1,33 +1,71 @@
-//Importing variables from the data.js
+// ------------------------------------------- Importing variables from the data.js ----------------------------------------------------- //
 import { BOOKS_PER_PAGE, authors, genres, books } from './data.js';
 
-// create const for matches
-const matches = books
+// ------------------------------------------- Retrieved elements from the DOM using query Selectors ------------------------------------ //
+const settings_Button = document.querySelector('[data-header-settings]')
+const settings_Overlay = document.querySelector('[data-settings-overlay]')
+const settings_Form = document.querySelector('[data-settings-form]')
+const settings_Theme = document.querySelector('[data-settings-theme]')
+const settings_Cancel = document.querySelector('[data-settings-cancel]')
 
-// create let for pages -- as it changes further in the code
+// ------------------------------------------- Day & Night Option ----------------------------------------------------------------------- //
+// ------------------------------------------- Event listner allowing you to click option to show theme --------------------------------- //
+settings_Button.addEventListener('click', () => {
+    settings_Overlay.showModal()
+})
+// ------------------------------------------- Event listener to click cancel -----------------------------------=----------------------- //
+settings_Cancel.addEventListener('click', () => { 
+    settings_Overlay.close()
+})
+
+// ------------------------------------------- The css object defines two themes, 'day' and 'night' ------------------------------------- //
+const css = {
+    day : ['255, 255, 255', '10, 10, 20'],
+    night: ['10, 10, 20', '255, 255, 255']
+}
+
+/**
+ * The Window interface's matchMedia() method returns a new MediaQueryList object that can then be used to determine if the document
+ * matches the media query string, as well as to monitor the document to detect when it matches (or stops matching) that media query.
+ */
+
+// --- The value of the settingsTheme input is determined based on whether the user's preferred color scheme is dark or not. ------------ //
+settings_Theme.value = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day'
+
+// --- When the form is submitted, the selected object is created by converting the form data to an object using Object.fromEntries(). -- //
+settings_Form.addEventListener('submit', (event) => { 
+    event.preventDefault()
+    const form_Submit = new FormData(event.target)
+    const selected = Object.fromEntries(form_Submit)
+
+/**
+ * Depending on the theme selected, the --color-light and --color-dark CSS variables are updated with the corresponding light and dark 
+ * color values from the css object -- root --- //
+ */ 
+if (selected.theme === 'night') {  
+        document.documentElement.style.setProperty('--color-light', css[selected.theme][0])
+        document.documentElement.style.setProperty('--color-dark', css[selected.theme][1])     
+    } else if (selected.theme === 'day') {
+        document.documentElement.style.setProperty('--color-light', css[selected.theme][0])
+        document.documentElement.style.setProperty('--color-dark', css[selected.theme][1])
+    }
+    settings_Overlay.close()
+})
+// ------------------------------------------- End of color theme ----------------------------------------------------------------------- //
+
+// ------------------------------------------- Create let for pages -- as it changes further in the code -------------------------------- //
 let page = 1;
 
 // add curly brackets --- replace range with page
 if (!books && !Array.isArray(books)) {throw new Error('Source required')} 
 if (!page && page.length < 2) {throw new Error('Range must be an array with two numbers')}
 
-// create const variable name for day and night
-const day = {
-    dark: '10, 10, 20',
-    light: '255, 255, 255',
-}
-
-const night = {
-    dark: '255, 255, 255',
-    light: '10, 10, 20',
-}
-
 const fragment = document.createDocumentFragment()
 // create let variable for startIndex and endIndex 
-let startIndex = 0;                                  
-let endIndex = 36;                                
+let start_Index = 0;                                  
+let end_Index = 36;                                
 // conclude let variable to extracted   
-const extracted = books.slice(startIndex, endIndex)
+const extracted = books.slice(start_Index, end_Index)
 
 // for loop to view books - imported data from data.js
 for (let i = 0; i < extracted.length; i++) {          
@@ -41,7 +79,7 @@ for (let i = 0; i < extracted.length; i++) {
     preview.dataset.description = books[i].description
     preview.dataset.genre = books[i].genres
 
-    preview.innerHTML=// --------------- HTML structure ---------------------------
+    preview.innerHTML= // ------------------------------------------ HTML structure ------------------------------------------------- //
     `<div>
      <image class='preview__image' src="${books[i].image}" alt="book pic"}/>
      </div>
@@ -52,88 +90,36 @@ for (let i = 0; i < extracted.length; i++) {
 
     fragment.appendChild(preview)
 }
-// display fragment in data-list-items
+
+// ------------------------------------------- Display fragment in data-list-items -------------------------------------------------- //
 const book_list_1 = document.querySelector('[data-list-items]') 
 book_list_1.appendChild(fragment)
 
-// create search button with data stored in data-header-search = imported from data.js
+// ------------------------------------------- Create search button with data stored in data-header-search = imported from data.js -- //
 const search_button = document.querySelector("[data-header-search]");
     search_button.addEventListener('click', () => {
     document.querySelector("[data-search-overlay]").style.display = "block";
 })
 
-// create cancel button with data stored in data-search-cancel = imported from data.js
+// ------------------------------------------- Create cancel button with data stored in data-search-cancel = imported from data.js -- //
 const search_cancel = document.querySelector("[data-search-cancel]");
     search_cancel.addEventListener('click', () => {
     document.querySelector("[data-search-overlay]").style.display = "none";
 })
 
-// create settings button with data stored in data-header-settings = imported from data.js
+// ------------------------------------------- Create settings button with data stored in data-header-settings = imported from data.js - //
 const setting_button = document.querySelector("[data-header-settings]")
     setting_button.addEventListener('click', () => {
     document.querySelector("[data-settings-overlay]").style.display = "block";
 })
 
-// create cancel settings button with data stored in data-setting-cancel = imported from data.js
+// ------------------------------------------- Create cancel settings button with data stored in data-setting-cancel = imported from data.js - //
 const setting_cancel = document.querySelector('[data-settings-cancel]')
     setting_cancel.addEventListener('click', () => {
     document.querySelector("[data-settings-overlay]").style.display = "none";
 })
 
-// create data setting theme with data stored in data-setting-theme = imported from data.js
-const data_settings_theme = document.querySelector('[data-settings-theme]');
-
-// create save button click function
-const save_button = document.querySelector("body > dialog:nth-child(5) > div > div > button.overlay__button.overlay__button_primary");
-save_button.addEventListener('click', handleClick);
-// data_search_genres.appendChild(genres)
-
-// Theme event function - click handle function
-function handleClick(event) {
-/** 
- * The preventDefault() method of the Event interface tells the user agent that if the event 
- * does not get explicitly handled, its default action should not be taken as it normally would be.
- */
-    event.preventDefault();
-    // create variable assigning data from variable that was created data_settings_theme 
-    const value = data_settings_theme.value;
-    const body = document.querySelector('body');
-    const overlay = document.querySelector("[data-settings-overlay]");
-    
-    // Type of themes colors in an object
-    const themes = {
-        day: {
-            dark: '#000000',
-            light: '#FFFFFF'
-        },
-        night: {
-            dark: '#111111',
-            light: '#CCCCCC'
-        }
-    };
-
-    // if statement day
-    if (value === 'day') {
-        // set color included from the object above
-        setThemeColors(body, themes.day.dark, themes.day.light);
-        overlay.style.display = "none";
-    }
-
-    // if statement night
-    if (value === 'night') {
-        // set color included from the object above
-        setThemeColors(body, themes.night.dark, themes.night.light);
-        overlay.style.display = "none";
-    }
-}
-// clickHandler set theme colors from root in css file
-function setThemeColors(element, darkColor, lightColor) {
-    element.style.setProperty('--color-dark', darkColor);
-    element.style.setProperty('--color-light', lightColor);
-}
-// ------------------------------------------------------------------------------------------------------------------------------------//
-
-// create variable to collect data from html to place them specifically 
+// ------------------------------------------- Create variable to collect data from html to place them specifically ------------------------- // 
 const authorSelect = document.querySelector("[data-search-authors]");
 const genreSelect = document.querySelector("[data-search-genres]");
 
@@ -145,7 +131,7 @@ const genreSelect = document.querySelector("[data-search-genres]");
  * to the enumerable string-keyed property key-value pairs found directly upon object.
  */
 
-// Object.entries() is used to iterate over the authors and genre in an arrow function
+// ------------------------------------------- Object.entries() is used to iterate over the authors and genre in an arrow function ---------- //
 Object.entries(authors).forEach(([authorId, authorName]) => {
     const optionElement = createOptionElement(authorId, authorName);
     authorSelect.appendChild(optionElement);
@@ -156,7 +142,7 @@ Object.entries(genres).forEach(([genreId, genreName]) => {
     genreSelect.appendChild(optionElement);
 });
 
-// function with paramaters - creating an empty selector for user to direct what the user wants 
+// ------------------------------------------- Function with paramaters - creating an empty selector for user to direct what the user wants - // 
 function createOptionElement(value, text) {
     // creating option in html
     const optionElement = document.createElement('option');
@@ -165,16 +151,16 @@ function createOptionElement(value, text) {
     return optionElement;
 }
 
-// create event function for details to display
+// ------------------------------------------- Create event function for details to display ------------------------------------------------- //
 const detailsToggle = (event) => {  
-    // create variable to call data-"key" in html
+    // ------------------------------------------- Create variable to call data-"key" in html ------------------------------------------- //
     const overlay1 = document.querySelector('[data-list-active]');
     const title = document.querySelector('[data-list-title]')
     const subtitle = document.querySelector('[data-list-subtitle]')
     const description = document.querySelector('[data-list-description]')
     const image1 = document.querySelector('[data-list-image]')
     const imageblur = document.querySelector('[data-list-blur]')
-    // if statement to display the books id from data.js in the html
+    // ------------------------------------------- if statement to display the books id from data.js in the html ------------------------ //
     event.dataset.id ? overlay1.style.display = "block" : undefined;
     event.dataset.description ? description.innerHTML = event.dataset.description : undefined;
     event.dataset.subtitle ? subtitle.innerHTML = event.dataset.subtitle : undefined;
@@ -182,40 +168,40 @@ const detailsToggle = (event) => {
     event.dataset.image ? image1.setAttribute ('src', event.dataset.image) : undefined;
     event.dataset.image ? imageblur.setAttribute ('src', event.dataset.image) : undefined;
 };
-// click function to close details
-const detailsClose = document.querySelector('[data-list-close]')    
-    detailsClose.addEventListener('click', () => {
+// ------------------------------------------- Click function to close details -------------------------------------------------------------- //
+const details_Close = document.querySelector('[data-list-close]')    
+    details_Close.addEventListener('click', () => {
     document.querySelector("[data-list-active]").style.display = "none";
 });
 
-// add event lisnter to click on specific data-list
-const bookclick = document.querySelector('[data-list-items]')
-bookclick.addEventListener('click', detailsToggle)
+// ------------------------------------------- Add event lisnter to click on specific data-list --------------------------------------------- //
+const book_click = document.querySelector('[data-list-items]')
+book_click.addEventListener('click', detailsToggle)
 
-// show more books - selecting button in html
-const showMoreButton = document.querySelector('[data-list-button]')
+// ------------------------------------------- Show more books - selecting button in html --------------------------------------------------- //
+const show_More_Button = document.querySelector('[data-list-button]')
 
-// variable structure of books when clicking more
-const numItemsToShow = Math.min(books.length - endIndex,)
+// ------------------------------------------- Variable structure of books when clicking more ----------------------------------------------- //
+const num_Items_To_Show = Math.min(books.length - end_Index,)
 
-// variable structure to display amount of books when clicking more
-const showMoreButtonText = `Show More (${numItemsToShow})`
+// ------------------------------------------- Variable structure to display amount of books when clicking more ----------------------------- //
+const show_More_Button_Text = `Show More (${num_Items_To_Show})`
 
-// .textContent placing variable in HTML 
-showMoreButton.textContent = showMoreButtonText
+// ------------------------------------------- .textContent placing variable in HTML -------------------------------------------------------- //
+show_More_Button.textContent = show_More_Button_Text
 
-// event lisener to click the more button and display more books 
-showMoreButton.addEventListener('click', () => {         
+// ------------------------------------------- Event lisener to click the more button and display more books -------------------------------- //
+show_More_Button.addEventListener('click', () => {         
     const fragment = document.createDocumentFragment()
-    startIndex += 36;
-    endIndex += 36;
-    const startIndex1 = startIndex
-    const endIndex1 = endIndex
-    console.log(startIndex1)
-    console.log(endIndex1)
-    const extracted = books.slice(startIndex1, endIndex1)
+    start_Index += 36;
+    end_Index += 36;
+    const start_Index_1 = start_Index
+    const end_Index_1 = end_Index
+    console.log(start_Index_1)
+    console.log(end_Index_1)
+    const extracted = books.slice(start_Index_1, end_Index_1)
 
-    // for loop to contuninue displaying all books, images, title, id, description, plublished in object imported from data.js file
+    // for loop to contuninue displaying all books, images, title, id, description, plublished in object imported from data.js file ---- //
     for (const {author ,image, title, id , description, published} of extracted) {
         // preview creating an discription list in html and previewing all information stored
         const preview = document.createElement('dl')
@@ -226,7 +212,7 @@ showMoreButton.addEventListener('click', () => {
         preview.dataset.subtitle = `${authors[author]} (${(new Date(published)).getFullYear()})`
         preview.dataset.description = description
         
-        preview.innerHTML= // --------------- HTML structure ---------------------------
+        preview.innerHTML= // ------------------------------------------ HTML structure ------------------------------------------------- //
         `<div>
          <image class='preview__image' src="${image}" alt="book pic"}/>
          </div>
@@ -234,10 +220,11 @@ showMoreButton.addEventListener('click', () => {
          <dt class='preview__title'>${title}<dt>
          <dt class='preview__author'> By ${authors[author]}</dt>
          </div>`
-        // appendChild allowing preview variable to display in fragment
+        // ------------------------------------------- appendChild allowing preview variable to display in fragment --------------------- //
         fragment.appendChild(preview)
     }
     
     const booklist1 = document.querySelector('[data-list-items]') 
     booklist1.appendChild(fragment)
 });
+// -------------------------------------------------- End of show more button ---------------------------------------------------------------- //
